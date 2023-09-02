b365ae496684443c2562d5d55e561b59aae535c5
jcf1 <john.c.freeman17@gmail.com>
2023-09-02 13:17:28 -0400

    Integrated ffmpeg to create clips from video

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,50 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import ReactPlayer from 'react-player';
 import Slider from '@mui/material/Slider';
 import { OnProgressProps } from 'react-player/base';
+import { FFmpeg } from '@ffmpeg/ffmpeg';
+import { toBlobURL, fetchFile } from '@ffmpeg/util';
+import { saveAs } from 'file-saver';
 
 function App() {
   const minDistance = 1;
   const maxDistance = 60;
 
+  const [loaded, setLoaded] = useState(false);
   const [url, setUrl] = useState('');
   const [range, setRange] = useState([0,0]);
   const [duration, setDuration] = useState(0);
 
   const playerRef = useRef<ReactPlayer>(null);
   const uploadRef = useRef<HTMLInputElement>(null);
+  const ffmpegRef = useRef(new FFmpeg());
+  const messageRef = useRef<HTMLParagraphElement | null>(null);
+
+  useEffect(() => {
+    load();
+  }, []);
+
+  const load = async () => {
+    const baseURL = "https://unpkg.com/@ffmpeg/core-mt@0.12.2/dist/esm";
+    const ffmpeg = ffmpegRef.current;
+    ffmpeg.on("log", ({ message }) => {
+      if (messageRef.current) messageRef.current.innerHTML = message;
+    });
+    // toBlobURL is used to bypass CORS issue, urls with the same
+    // domain can be used directly.
+    await ffmpeg.load({
+      coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, "text/javascript"),
+      wasmURL: await toBlobURL(
+        `${baseURL}/ffmpeg-core.wasm`,
+        "application/wasm"
+      ),
+      workerURL: await toBlobURL(
+        `${baseURL}/ffmpeg-core.worker.js`,
+        "text/javascript"
+      ),
+    });
+    setLoaded(true);
+  };
   
   const timeStr = (seconds: number) => {
     let minutes = seconds / 60;
@@ -20,6 +52,17 @@ function App() {
     minutes = Math.floor(minutes % 60);
     seconds = Math.floor(seconds % 60);
     return String(hours) + ":" + String(minutes).padStart(2,'0') + ":" + String(seconds).padStart(2,'0');
+  }
+
+  const clipSegment = async () => {
+    const ffmpeg = ffmpegRef.current;
+    const start = timeStr(range[0]);
+    const len = range[1] - range[0];
+    await ffmpeg.writeFile('input.mp4', await fetchFile(url));
+    await ffmpeg.exec(['-ss', start, '-i', 'input.mp4', '-t', len.toString(), 'output.mp4']);
+    const data = await ffmpeg.readFile('output.mp4');
+    const blob = new Blob([data], {"type" : "video\/mp4"})
+    saveAs(blob, "test.mp4");    
   }
 
   const fileTest = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -73,7 +116,7 @@ function App() {
     playerRef.current?.seekTo(minRange / duration);
   };
 
-  return (
+  return loaded ? (
     <div className="grid justify-center justify-items-center h-full w-full">
 
       <div className="flex w-4/5 h-auto justify-center items-center">
@@ -115,8 +158,14 @@ function App() {
         </div>
       </div>
 
-      <input className="is-hidden" ref={uploadRef} type="file" accept=".mp4" onChange={(e) => fileTest(e)}/>
-      <input type="button" value="Browse..." onClick={() => uploadRef.current?.click()}/>
+      <input className="invisible" ref={uploadRef} type="file" accept=".mp4" onChange={(e) => fileTest(e)}/>
+      <input className="w-4/5 h-auto text-white bg-blue-600" type="button" value="Browse..." onClick={() => uploadRef.current?.click()}/>
+
+      <input className="w-4/5 h-auto text-white bg-red-500" type="button" value="Clip" onClick={clipSegment}/>
+    </div>
+  ) : (
+    <div>
+      LOADING
     </div>
   )
 }
